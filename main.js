a7ad92114d5d1e6c314a7a0e5ece9e932a66bc05
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-01-19 04:14:50 +0500

    added files

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
 //destructuring i.e bringing things out of electron
 const {app, BrowserWindow } = require('electron')
 
+//set enviournment 
+process.env.NODE_ENV = 'development'
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
+
+
 let mainWindow 
 function createMainWindow(){
     mainWindow = new BrowserWindow({
         title: 'Image Shrink',
         width: 1680,
         height: 940,
-        alwaysOnTop: true,
+        // alwaysOnTop: true, 
         darkTheme: true,
-        opacity: 1,
+        opacity: isDev ? 1 : 0.5,
+        icon: './assets/icons/shrink.png',
+        resizable: isDev ? true : false,
+
     })
-     
-    mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+    // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+    mainWindow.loadFile('./app/index.html')
 }
-
-
+ 
 app.on('ready', createMainWindow)
+
+app.on('window-all-closed', () => {
+
+    if (!isMac){
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0){
+        createMainWindow()
+    }
+})
+
+app.allowRendererProcessReuse = true
