69a8181bff100f7c2cf76ca266b8486119e8b139
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-01-26 18:32:24 +0500

    Update renderer.js

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,4 +1,11 @@
 const path = require('path')
 const os = require('os')
+
+const form = document.getElementById('image-form')
+const slider = document.getElementById('slider')
+const img = document.getElementById('img')
+
+
+
 document.getElementById('output-path').innerText = path.join(os.homedir(), "Image_Shrink")
 
