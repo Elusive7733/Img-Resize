f941307d648f0b2bfc3a1268e40b208fbddceca2
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-01-26 14:14:31 +0500

    Gsap finally working

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,6 @@
-// import { gsap } from 'gsap';
-
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.4 });
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1.5");
 tl.fromTo(".landing", { opacity: 0 }, { opacity: 1, duration: 1 });
-console.log("here")
